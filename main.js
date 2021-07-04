d65d7a771b6e615eedcc71d082b474cc4c2065c0
Tirupathi Gracy Hephzibah <50313861+hephsi@users.noreply.github.com>
2021-07-04 23:02:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ function updateScore(card, activePlayer) {
 function showCard(card, activePlayer) {
     if (activePlayer['score'] <= 21) {
       let cardImage = document.createElement('IMG');
-      cardImage.src = '/images/${card}.png';
+      cardImage.src = 'https://github.com/hephsi/BlackJack-Game/blob/main/images/${card}.png';
       document.querySelector(activePlayer['div']).appendChild(cardImage);
       hitSound.play();
     }
